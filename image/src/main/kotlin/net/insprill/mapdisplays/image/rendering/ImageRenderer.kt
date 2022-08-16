5c8eea8ce8e6a0ab4dd1b89f459fb3e39492cb30
Pierce Thompson <insprill@gmail.com>
2022-08-16 15:18:57 -0400

    Don't re-draw images

--- image/src/main/kotlin/net/insprill/mapdisplays/image/rendering/ImageRenderer.kt
+++ image/src/main/kotlin/net/insprill/mapdisplays/image/rendering/ImageRenderer.kt
@@ -8,12 +8,16 @@ import org.bukkit.map.MapView
 
 class ImageRenderer(private val image: Image) : MapRenderer() {
 
+    private var drawn = false
+
     override fun render(map: MapView, canvas: MapCanvas, player: Player) {
+        if (drawn) return
         for (pixel in image.pixels) {
             for (coord in pixel.mapCoords) {
                 canvas.setPixel(coord.x, coord.y, pixel.color)
             }
         }
+        drawn = true
     }
 
 }
