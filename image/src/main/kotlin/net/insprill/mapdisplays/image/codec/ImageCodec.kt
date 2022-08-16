58597e0499706ac9e6e9d0b0e79284df50bf1d08
Pierce Thompson <insprill@gmail.com>
2022-08-16 14:01:20 -0400

    Fix image codec not supporting more than 128 pixels or 128 pixel coordinates

--- image/src/main/kotlin/net/insprill/mapdisplays/image/codec/ImageCodec.kt
+++ image/src/main/kotlin/net/insprill/mapdisplays/image/codec/ImageCodec.kt
@@ -1,5 +1,6 @@
 package net.insprill.mapdisplays.image.codec
 
+import com.google.common.primitives.Ints
 import net.insprill.mapdisplays.core.MapCoord
 import net.insprill.mapdisplays.core.codec.Codec
 import net.insprill.mapdisplays.core.exception.DecodeException
@@ -7,6 +8,8 @@ import net.insprill.mapdisplays.image.Image
 import net.insprill.mapdisplays.image.Pixel
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
+import java.io.InputStream
+import java.io.OutputStream
 
 object ImageCodec : Codec<Image> {
 
@@ -15,10 +18,10 @@ object ImageCodec : Codec<Image> {
     override fun encode(obj: Image): ByteArrayOutputStream {
         val output = ByteArrayOutputStream()
         output.write(CODEC_VERSION)
-        output.write(obj.pixels.size)
+        output.writeLarge(obj.pixels.size)
         for (pixel in obj.pixels) {
             output.write(pixel.color.toInt())
-            output.write(pixel.mapCoords.size)
+            output.writeLarge(pixel.mapCoords.size)
             for (coord in pixel.mapCoords) {
                 output.write(coord.x)
                 output.write(coord.y)
@@ -33,11 +36,11 @@ object ImageCodec : Codec<Image> {
         if (codec != CODEC_VERSION)
             throw DecodeException("Unknown codec version $codec")
         val pixels = ArrayList<Pixel>()
-        val pixelCount = input.read()
+        val pixelCount = input.readLarge()
         repeat(pixelCount) {
             val pixelColor = input.read()
             val pixel = Pixel(pixelColor.toByte())
-            val coordCount = input.read()
+            val coordCount = input.readLarge()
             repeat(coordCount) {
                 val x = input.read()
                 val y = input.read()
@@ -46,6 +49,24 @@ object ImageCodec : Codec<Image> {
             pixels.add(pixel)
         }
         return Image(pixels)
+    }
+
+    private fun OutputStream.writeLarge(i: Int) {
+        if (i > Byte.MAX_VALUE || i < Byte.MIN_VALUE) {
+            this.write(1)
+            this.write(Ints.toByteArray(i))
+        } else {
+            this.write(0)
+            this.write(i)
+        }
+    }
+
+    private fun InputStream.readLarge(): Int {
+        return if (this.read() == 1) {
+            Ints.fromByteArray(this.readNBytes(4))
+        } else {
+            this.read()
+        }
     }
 
 }
