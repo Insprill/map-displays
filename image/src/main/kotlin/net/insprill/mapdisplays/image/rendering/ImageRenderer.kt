2ba7e6e8a988c03416a99e157432240e223749a1
Pierce Thompson <insprill@gmail.com>
2022-08-16 12:10:16 -0400

    Add ImageRenderer

---
+++ image/src/main/kotlin/net/insprill/mapdisplays/image/rendering/ImageRenderer.kt
@@ -0,0 +1,20 @@
+package net.insprill.mapdisplays.image.rendering
+
+import net.insprill.mapdisplays.image.Image
+import org.bukkit.entity.Player
+import org.bukkit.map.MapCanvas
+import org.bukkit.map.MapRenderer
+import org.bukkit.map.MapView
+
+class ImageRenderer(private val image: Image) : MapRenderer() {
+
+    override fun render(map: MapView?, canvas: MapCanvas?, player: Player?) {
+        if (canvas == null) return
+        for (pixel in image.pixels) {
+            for (coord in pixel.mapCoords) {
+                canvas.setPixel(coord.x, coord.y, pixel.color)
+            }
+        }
+    }
+
+}
