cc3dd438b772c49eb551fc5b499daf5e6cd78dae
Pierce Thompson <insprill@gmail.com>
2022-08-16 12:29:03 -0400

    Override #equals, #hashCode, and #toString in Image, Pixel, and MapCoord

--- image/src/main/kotlin/net/insprill/mapdisplays/image/Image.kt
+++ image/src/main/kotlin/net/insprill/mapdisplays/image/Image.kt
@@ -31,4 +31,23 @@ class Image {
         this.pixels = ArrayList(pixels)
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as net.insprill.mapdisplays.image.Image
+
+        if (pixels != other.pixels) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return pixels.hashCode()
+    }
+
+    override fun toString(): String {
+        return "Image(pixels=$pixels)"
+    }
+
 }
--- image/src/main/kotlin/net/insprill/mapdisplays/image/Pixel.kt
+++ image/src/main/kotlin/net/insprill/mapdisplays/image/Pixel.kt
@@ -10,4 +10,26 @@ class Pixel(val color: Byte) {
         mapCoords.add(mapCoord)
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Pixel
+
+        if (color != other.color) return false
+        if (mapCoords != other.mapCoords) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = color.toInt()
+        result = 31 * result + mapCoords.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        return "Pixel(color=$color, mapCoords=$mapCoords)"
+    }
+
 }
