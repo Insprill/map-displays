81631203a3b7511d4a91047d14f253df45b58616
Pierce Thompson <insprill@gmail.com>
2022-08-16 11:55:57 -0400

    Add EncodeException and DecodeException

---
+++ core/src/main/kotlin/net/insprill/mapdisplays/core/exception/DecodeException.kt
@@ -0,0 +1,5 @@
+package net.insprill.mapdisplays.core.exception
+
+import java.io.IOException
+
+class DecodeException(msg: String) : IOException(msg)
---
+++ core/src/main/kotlin/net/insprill/mapdisplays/core/exception/EncodeException.kt
@@ -0,0 +1,5 @@
+package net.insprill.mapdisplays.core.exception
+
+import java.io.IOException
+
+class EncodeException(msg: String) : IOException(msg)
