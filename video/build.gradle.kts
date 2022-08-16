514cc4a0c78537ac127a592c5aac7cbf1e72efbe
Pierce Thompson <insprill@gmail.com>
2022-08-16 17:21:30 -0400

    Add jcodec as a video dependency

--- video/build.gradle.kts
+++ video/build.gradle.kts
@@ -6,5 +6,7 @@ version = project.property("video.version")!!
 
 dependencies {
     compileOnly("org.spigotmc:spigot-api:${project.property("common.spigot-api.version")!!}")
+    implementation("org.jcodec:jcodec:0.2.5")
+    implementation("org.jcodec:jcodec-javase:0.2.5")
     implementation(project(":core"))
 }
